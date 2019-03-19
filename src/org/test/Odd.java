708514c3f126804187e6d9eb8985529d0061006d
venkat2400 <48574897+venkat2400@users.noreply.github.com>
2019-03-19 12:17:56 +0530

    added a odd class

---
+++ src/org/test/Odd.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class Odd {
+
+	public static void main(String[] args) {
+		for (int i = 0; i <= 100; i++) {
+			if (i % 2 == 1) {
+				System.out.println(i);
+
+			}
+
+		}
+	}
+}
